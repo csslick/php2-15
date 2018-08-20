364c0448062ac3f52e9854f09ffed4eefcc38256
web <digitone@naver.com>
2018-08-20 17:58:24 +0900

    select list

---
+++ 15-4-6_linkBtn.php
@@ -0,0 +1,17 @@
+<?php
+  include_once "./15-1_connectDB.php";
+
+  $sql = "SELECT * FROM customer1000";
+  $res = $conn->query($sql);
+
+  $numView = 50;
+  $totalRecord = $res->num_rows;
+
+  // 페이지 수
+  $numPage = ceil($totalRecord / $numView);
+
+  for($i = 1; $i < $numPage; $i++){ ?>
+    <a href="./15-4-5_selectLimit.php?page=<?=$i?>">
+      <?=$i?>
+    </a>
+<?php } ?>
